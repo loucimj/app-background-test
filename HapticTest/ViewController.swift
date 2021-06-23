a4039f54db80d1dfa8df3e7a628a88f7f4107b7c
Javier Loucim <jloucim@paypal.com>
2021-06-23 16:04:36 -0300

    added background logic

--- HapticTest/ViewController.swift
+++ HapticTest/ViewController.swift
@@ -6,14 +6,105 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
+    var backgroundTask: UIBackgroundTaskIdentifier = .invalid
+
+    var startTimestamp: Date?
+    var endTimeStamp: Date?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.gray
+        checkPermission()
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(reinstateBackgroundTask), name: UIApplication.didBecomeActiveNotification, object: nil)
+
+        simulateUseCase()
+    }
+
+    func startBackgroundTask() {
+        startTimestamp = Date()
+        backgroundTask = UIApplication.shared.beginBackgroundTask {
+            self.finishBackgroundTask()
+        }
+    }
+
+    func finishBackgroundTask() {
+        UIApplication.shared.endBackgroundTask(self.backgroundTask)
+        self.backgroundTask = .invalid
+
+    }
+    func simulateUseCase() {
+        startBackgroundTask()
+        DispatchQueue.global().asyncAfter(deadline: .now() + .seconds(3)) {
+            self.sendLocalNotification()
+            print("Background time remaining = \(UIApplication.shared.backgroundTimeRemaining) seconds")
+            print("haptics! \(Calendar.current.dateComponents([.second], from: self.startTimestamp ?? Date(), to: Date()).second ?? 0)")
+            self.finishBackgroundTask()
+        }
+    }
+
+    @objc func reinstateBackgroundTask() {
+        guard backgroundTask == .invalid else { return }
+        simulateUseCase()
+    }
+
+    func sendLocalNotification() {
+        let notification = UNMutableNotificationContent()
+        notification.title = "Titulo"
+        notification.subtitle = "Haptics happened!"
+        notification.userInfo["identity_id"] = UUID().uuidString
+        notification.categoryIdentifier = "alarm"
+        notification.sound = UNNotificationSound.defaultCritical
+        self.scheduleLocalNotification(with: notification)
+    }
+
+    func checkPermission() {
+        UNUserNotificationCenter.current().getNotificationSettings { (notificationSettings) in
+            switch notificationSettings.authorizationStatus {
+            case .notDetermined:
+                self.requestPermission()
+            case .authorized: break
+            case .denied:
+            print("App don't have permissoon to display notifications")
+            case .provisional:
+                print("App don't have permissoon to display notifications")
+            case .ephemeral:
+                print("App don't have permissoon to display notifications")
+            @unknown default:
+                print("App don't have permissoon to display notifications")
+            }
+        }
+    }
+
+    func requestPermission() {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+
+            if let error = error {
+                print("\(error)")
+            }
+        }
+    }
+
+    private func scheduleLocalNotification(with content: UNMutableNotificationContent) {
+        let trigger = UNTimeIntervalNotificationTrigger(
+            timeInterval: 0.1,
+            repeats: false
+        )
+        let request = UNNotificationRequest(
+            identifier: UUID().uuidString,
+            content: content,
+            trigger: trigger
+        )
+
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
 
 }
 
